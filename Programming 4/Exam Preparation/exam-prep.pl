b6306995f84488bc97b13ad95a7a23055f043cf0
grayson-orr <graysonorr95@outlook.com>
2019-06-19 09:07:32 +1200

    Add exam-prep.pl

---
+++ Programming 4/Exam Preparation/exam-prep.pl
@@ -0,0 +1,27 @@
+male(james1).
+male(charles1).
+male(charles2).
+male(james2).
+male(george1).
+
+female(catherine).
+female(elizabeth).
+female(sophia).
+
+parent(charles1, james1).
+parent(elizabeth, james1).
+parent(charles2, charles1).
+parent(catherine, charles1).
+parent(james2, charles1).
+parent(sophia, elizabeth).
+parent(george1, sophia).
+
+% Knowledge base
+likes(mary, food).
+likes(mary, wine).
+likes(john, wine).
+likes(john, mary).
+
+likes(mary, food). % yes
+likes(john, wine). % yes
+likes(john, food). % no
